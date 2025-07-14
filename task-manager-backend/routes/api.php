ea88b085f1f7f6719516ac0e7ebb90bdc0e49cc8
Edmund <edmundgachanja@gmail.com>
2025-07-14 23:50:52 +0300

    fix:bug

--- task-manager-backend/routes/api.php
+++ task-manager-backend/routes/api.php
@@ -23,20 +23,20 @@ use App\Http\Controllers\UserController;
 Route::post('/signup', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-// Protected routes
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
 
-    // Tasks
-    Route::get('/tasks', [TaskController::class, 'index']);
-    Route::post('/tasks', [TaskController::class, 'store']);
-    Route::put('/tasks/{task}', [TaskController::class, 'update']);
-    Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
-    Route::get('/user-tasks/{userId}', [TaskController::class, 'userTasks']);
+Route::post('/logout', [AuthController::class, 'logout']);
 
-    //users
-    Route::get('/users', [UserController::class, 'index']);
-    Route::post('/users', [UserController::class, 'store']);
-    Route::put('/users/{id}', [UserController::class, 'update']);
-    Route::delete('/users/{id}', [UserController::class, 'destroy']);
-});
+// Tasks
+Route::get('/tasks', [TaskController::class, 'index']);
+Route::post('/tasks', [TaskController::class, 'store']);
+Route::put('/tasks/{task}', [TaskController::class, 'update']);
+Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
+Route::get('/user-tasks/{userId}', [TaskController::class, 'userTasks']);
+
+//users
+Route::get('/users', [UserController::class, 'index']);
+Route::post('/users', [UserController::class, 'store']);
+Route::put('/users/{id}', [UserController::class, 'update']);
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+
